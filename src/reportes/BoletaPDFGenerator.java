3ff8368a372eca3f88cfa01e809946fa657cb50f
jose1801 <153047562+JLWhiter@users.noreply.github.com>
2026-06-10 11:45:30 -0500

    se arreglo un bug sobre el login y themes

--- src/reportes/BoletaPDFGenerator.java
+++ src/reportes/BoletaPDFGenerator.java
@@ -21,6 +21,11 @@ public class BoletaPDFGenerator {
             PdfWriter.getInstance(doc, new FileOutputStream(ruta));
             doc.open();
 
+            // ----------------------------------------------------------
+            // Aquí puedes agregar el LOGO de la empresa (en formato PNG)
+            // ----------------------------------------------------------
+
+            // ENCABEZADO EMPRESA
             Paragraph empresa = new Paragraph("EMPRESA DE TRANSPORTES SOFTFKES S.A.", FONT_BOLD);
             empresa.setAlignment(Element.ALIGN_CENTER);
             doc.add(empresa);
